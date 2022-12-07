e686d174b4780173f354c87b040a07429ce2f023
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-12-06 21:57:45 -0500

    Update network.cpp

--- network.cpp
+++ network.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include "network.h"
+using namespace std;
+
+
+Profile::Profile(string usrn, string dspn){
+username=usrn;
+displayname=dspn;
+}
+
+Profile::Profile(){
+username="";
+displayname="";
+}
+
+string Profile::getUsername(){
+return username;
+}
+
+string Profile::getFullName(){
+return displayname + " (@" +username + ")";
+}
+
+void Profile::setDisplayName(string dspn){
+displayname=dspn;
+}
+
 

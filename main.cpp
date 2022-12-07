1dab380772126b22163f6a5e3f35b72e0ffc468f
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-12-06 21:54:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include "profile.h"
 #include <profile.cpp>
+#include "network.h"
+#include <network.cpp>
 using namespace std;
 
 int main() {
@@ -15,4 +17,20 @@ int main() {
     Profile p2("tarma1", "Tarma Roving");    
     cout << p2.getUsername() << endl; // tarma1
     cout << p2.getFullName() << endl; // Tarma Roving (@tarma1)
+}
+
+int main() {
+  Network nw;
+  cout << nw.addUser("mario", "Mario") << endl;     // true (1)
+  cout << nw.addUser("luigi", "Luigi") << endl;     // true (1)
+
+  cout << nw.addUser("mario", "Mario2") << endl;    // false (0)
+  cout << nw.addUser("mario 2", "Mario2") << endl;  // false (0)
+  cout << nw.addUser("mario-2", "Mario2") << endl;  // false (0)
+
+  for(int i = 2; i < 20; i++)
+      cout << nw.addUser("mario" + to_string(i), 
+                 "Mario" + to_string(i)) << endl;   // true (1)
+
+  cout << nw.addUser("yoshi", "Yoshi") << endl;     // false (0)
 }

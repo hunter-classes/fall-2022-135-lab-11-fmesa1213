f636ee1300399d48fa661033edf9dd3d729278a4
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-12-06 21:56:43 -0500

    Update network.h

--- network.h
+++ network.h
@@ -20,25 +20,3 @@ string getFullName();
 // Change display name
 void setDisplayName(string dspn);
 };
-Profile::Profile(string usrn, string dspn){
-username=usrn;
-displayname=dspn;
-}
-
-Profile::Profile(){
-username="";
-displayname="";
-}
-
-string Profile::getUsername(){
-return username;
-}
-
-string Profile::getFullName(){
-return displayname + " (@" +username + ")";
-}
-
-void Profile::setDisplayName(string dspn){
-displayname=dspn;
-}
-

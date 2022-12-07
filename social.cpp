5bfc39335382cd275ced62b031ac5d15ed4b78de
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-12-06 21:29:21 -0500

    Update social.cpp

--- social.cpp
+++ social.cpp
@@ -6,40 +6,8 @@ Assignment: Lab 11 A
 */
 
 #include<iostream>
+#include "social.h"
 using namespace std;
-
-class Profile {
-private:
-    string username;
-    string displayname;
-public:
-    // Profile constructor for a user (initializing 
-    // private variables username=usrn, displayname=dspn)
-    Profile(string usrn, string dspn);
-  {
-  username = usrn;
-    displayname = dspn;
-  }
-  
-  Profile();
-  {
-  username == " ";
-    displayname == " ";
-  }
-    string getUsername();
-  {
-  return username;
-  }
-    string getFullName();
-  {
-  string s = displayname + "(@"+username+")";
-    return s;
-  }
-    void setDisplayName(string dspn)
-    {
-    displayname = dspn;
-    }
-};
 
 int main() {
     Profile p1("marco", "Marco");    

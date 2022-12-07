66a8fa112540f1b013a18bcd563b80e191e4fe58
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-12-06 21:53:41 -0500

    Update network.h

--- network.h
+++ network.h
@@ -1,1 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Profile {
+private:
+string username;
+string displayname;
+
+public:
+// Profile constructor for a user (initializing
+// private variables username=usrn, displayname=dspn)
+
+Profile(string usrn, string dspn);
+// Default Profile constructor (username="", displayname="")
+Profile();
+// Return username
+string getUsername();
+// Return name in the format: "displayname (@username)"
+string getFullName();
+// Change display name
+void setDisplayName(string dspn);
+};
+Profile::Profile(string usrn, string dspn){
+username=usrn;
+displayname=dspn;
+}
+
+Profile::Profile(){
+username="";
+displayname="";
+}
+
+string Profile::getUsername(){
+return username;
+}
+
+string Profile::getFullName(){
+return displayname + " (@" +username + ")";
+}
+
+void Profile::setDisplayName(string dspn){
+displayname=dspn;
+}
 

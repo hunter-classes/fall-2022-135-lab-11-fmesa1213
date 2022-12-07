42f1d63bd0d442a3a2e1307fba8f3014dd38ba64
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-12-06 22:12:37 -0500

    Update social3.cpp

--- social3.cpp
+++ social3.cpp
@@ -4,3 +4,17 @@ include "profile.h"
 include "network.h"
 
   
+class Network {
+private:
+    static const int MAX_USERS = 20; 
+    int numUsers;                    
+    Profile profiles[MAX_USERS];     
+    bool following[MAX_USERS][MAX_USERS];    // new 
+    int findID (string usrn);
+public:
+    Network();
+    bool addUser(string usrn, string dspn);
+    bool follow(string usrn1, string usrn2); // new
+    void printDot();                         // new
+};
+
